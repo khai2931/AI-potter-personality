4ccfed1a773d92fb1dedd4979abb88604f2deefc
Khai-Huy Nguyen <khai2931@cs.washington.edu>
2024-09-20 21:10:53 -0700

    hiding the access token oopsie

--- example_code/hf_api_call.py
+++ example_code/hf_api_call.py
@@ -2,7 +2,7 @@ import requests
 
 # API_URL = "https://api-inference.huggingface.co/models/distilbert/distilgpt2"
 API_URL = "https://api-inference.huggingface.co/models/meta-llama/Meta-Llama-3.1-8B"
-headers = {"Authorization": "Bearer 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"}
+headers = {"Authorization": "Bearer hf_xxxxxxxxxxxxxxxxxxx"}
 
 payload = {
     "inputs": "Can you please let us know more details about your ",
